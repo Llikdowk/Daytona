65c1f198abc144267fa1da18c70518df27187c35
Llikdowk <guillermofm7@gmail.com>
2017-03-25 12:42:39 +0100

    untrack build folder

--- build/Debug/thirdparty/glm/test/gtx/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/WORKSPACE/C++/Daytona/thirdparty/glm/test/gtx
-# Build directory: D:/WORKSPACE/C++/Daytona/build/Debug/thirdparty/glm/test/gtx
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
